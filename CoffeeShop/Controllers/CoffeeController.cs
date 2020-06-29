253ebc31b49dbe9a537df6fa4b18e84d98566c4d
Crystal Elsey <hello@crystalelsey.com>
2020-06-29 18:06:00 -0500

    initial commit

---
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using CoffeeShop.Models;
+using CoffeeShop.Repositories;
+
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        private readonly CoffeeRepository _coffeeRepo;
+        public CoffeeController(IConfiguration configuration)
+        {
+            _coffeeRepo = new CoffeeRepository(configuration);
+        }
+
+        // https://localhost:5001/api/coffee/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_coffeeRepo.GetAll());
+        }
+
+        // https://localhost:5001/api/coffee/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var coffee = _coffeeRepo.Get(id);
+            if (coffee == null)
+            {
+                return NotFound();
+            }
+            return Ok(coffee);
+        }
+
+        // https://localhost:5001/api/coffee
+        [HttpPost]
+        public IActionResult Post(Coffee coffee)
+        {
+            _coffeeRepo.Add(coffee);
+            return CreatedAtAction("Get", new { id = coffee.Id }, coffee);
+        }
+
+        // https://localhost:5001/api/coffee/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Coffee coffee)
+        {
+            if (id != coffee.Id)
+            {
+                return BadRequest();
+            }
+            _coffeeRepo.Update(coffee);
+            return NoContent();
+        }
+
+        // https://localhost:5001/api/coffee/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _coffeeRepo.Delete(id);
+            return NoContent();
+        }
+    }
+}
